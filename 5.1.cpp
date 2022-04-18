746eeedaa6a0a1ec39b949486c2263da2a2cdef5
Orreoll <100588277+Orreoll@users.noreply.github.com>
2022-04-19 04:32:05 +0900

    Create 5.1.cpp

---
+++ 5.1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std ;
+
+float SquaredSum(float a, float b,float result)
+{
+    result=(a+b)*(a+b);
+    return result;
+}
+
+int main ()
+{
+    float c,d,result;
+    cout<<"Введите первое дробное число\n";
+    cin>>c;
+    cout<<"Введите второе дробное число\n";
+    cin>>d;
+    cout<<"Результат - "<<SquaredSum(c,d,result)<<"\n";
+}
