723f54e9a09042b57c8917d4b1b2d43ccf5a4883
Orreoll <100588277+Orreoll@users.noreply.github.com>
2022-04-19 04:32:41 +0900

    Create 5.2.cpp

---
+++ 5.2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+void Sort(int massive[])
+{
+  for (int i=0; i<10-1; i++)
+  {
+    for (int k=i+1; k<10; k++)
+    {
+      if (massive[i]<massive[k])
+      {
+        int a=massive[k];
+        massive[k]=massive[i];
+        massive[i]=a;
+      }
+    }
+  }
+}
+
+int main()
+{
+  int massive[10];
+  cout<<"Введите массив из 10 чисел"<<'\n';
+  for (int i=0; i<10; i++)
+  {
+    cout<<" ";
+    cin>>massive[i];
+  }
+  Sort(massive);
+  for (int k=0; k<10; k++)
+  {
+    cout<<massive[k]<<' ';
+  }
+  cout<<'\n';
+  return 0;
+}
