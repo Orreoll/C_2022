803d7eafa52a3717a3e7fe96b3bec0ba05e6a235
Orreoll <100588277+Orreoll@users.noreply.github.com>
2022-04-19 04:29:05 +0900

    Create 4.1.cpp

---
+++ 4.1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+int s;
+string b[10]={
+"ноль", "один","два","три","четыре","пять","шесть","семь","восемь","девять"
+};
+cout«"input num"«"\n";
+cin»s;
+cout«b[s];
+return 0;
+}
