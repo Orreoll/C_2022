0193a21b5f2f4d48ace064d6f824df55477386a5
Orreoll <100588277+Orreoll@users.noreply.github.com>
2022-04-20 23:32:21 +0900

    Update 5.1.cpp

--- 5.1.cpp
+++ 5.1.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
+#include <cmath>
+using namespace std;
 
-using namespace std ;
+    float SquaredSum(float a, float b) {
+    float d;
+    d = pow ((a + b), 2); //pow-возвращает значение base, возведенное в степень ехр
+    return d;
 
-float SquaredSum(float a, float b,float result)
-{
-    result=(a+b)*(a+b);
-    return result;
-}
+    }
 
-int main ()
-{
-    float c,d,result;
-    cout<<"Введите первое дробное число\n";
-    cin>>c;
-    cout<<"Введите второе дробное число\n";
-    cin>>d;
-    cout<<"Результат - "<<SquaredSum(c,d,result)<<"\n";
-}
+    int main()
+    {
+    float q, w, e, r;
+    cout << "первая дробь:"<<endl;
+    cin >> q >> w;
+    cout << "вторая дробь:"<<endl;
+    cin >> e >> r;
+
+    float t, y;
+    t = q / w;
+    y = e / r;
+    cout << SquaredSum(t, y) << endl;
+    return 0;
+    }
