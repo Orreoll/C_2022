53bce427fe9dbb71f098d5152af417c0d852e2f1
Orreoll <100588277+Orreoll@users.noreply.github.com>
2022-04-20 18:57:02 +0900

    Update 2.2.cpp

--- 2.2.cpp
+++ 2.2.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main()
 {
 int i=1,sum;
-while (i<1001)
+for (i;i<1000;i++)
 {
 if (i%7==0) {
 sum=sum+i;
 }
-i++;
+
 }
-cout«sum « "\n";
+cout << sum <<"\n";
 return 0;
 }
