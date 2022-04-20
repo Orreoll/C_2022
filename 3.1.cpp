bd226d3c241b348610339a753ca393d1bbb2c287
Orreoll <100588277+Orreoll@users.noreply.github.com>
2022-04-20 19:55:17 +0900

    Update 3.1.cpp

--- 3.1.cpp
+++ 3.1.cpp
@@ -1,24 +1,16 @@
 #include <iostream>
 #include <fstream>
-
+#include <iomanip>
 using namespace std;
-
-int main()
+int main ( )
 {
-int a, b;
-ofstream file("f");
-cout«"input A"«"\n";
-cin»a;
-cout«"input B"«"\n";
-cin»b;
-while(a<=b)
-{
-file«(a*3)«"\n";
-a++;
-}
-file.close();
+	int a, b; 
+	ofstream f; 
+	f.open ( " solbon " );
+	cout<<" b = "; cin>>b;
+	cout<<" a = "; cin>>a;
+	for ( a; a<=b; a++);
+	f <<(a*3)<<"\n";
+	f.close ( ); 
 return 0;
 }
-Файл не выбран
-Ещё
-
