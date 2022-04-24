46468a6631e3c07c9358362a4d9f0273dd344867
Orreoll <100588277+Orreoll@users.noreply.github.com>
2022-04-24 14:31:51 +0900

    Create 1.2.cpp

---
+++ 1.2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int b = 2;
+    float d = 2.55;
+    char v = '$';
+    short n = 2;
+    d = b+7;
+    d = n*5;
+    v++;
+    cout<< b<< "\n"<< d<< v<< "\n"<< d;
+    return 0;
+}
