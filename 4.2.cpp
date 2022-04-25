e0efac5c86927b2f3015246ea500cd01222d617b
Orreoll <100588277+Orreoll@users.noreply.github.com>
2022-04-26 05:10:31 +0900

    Update 4.2.cpp

--- 4.2.cpp
+++ 4.2.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include <cstring>
+
 using namespace std;
 
 int main()
 {
-char solbon[30];
-cin >> solbon;
-int a = 0;
-while (a < strlen (solbon))
-{
-int ascii = (int)solbon[a];
-if (ascii == 77)
-{
-ascii = (int)solbon[a] - 15; //для буквы z->a
-}
-else ascii = (int)solbon[a] + 1;
-cout << (char)ascii;
-++a;
-}
-char c;
-return 0;
+    short b, x=0, *ukaz;
+        char z[10];
+    cout << "Символ" << "\n";
+        cin >> z;
+    cout<<"значение"<< "\n";
+        b = strlen(z);
+    while (1){
+        ukaz=(short *) &z[x];
+            (*ukaz)++;
+         cout<<z[x];
+            if(b==x) break;
+            x++;
+        }
+    return 0;
 }
