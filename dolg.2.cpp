ad5a86afa70666ab4d4815e888a2b7e84a88db35
Orreoll <100588277+Orreoll@users.noreply.github.com>
2022-04-27 03:03:41 +0900

    Create dolg.2.cpp

---
+++ dolg.2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int N,i;
+float X;
+cout«"Введите длину массива N: "«"\n";
+cin»N;
+float Ratios[N];
+cout«"Введите делитель X: "«"\n";
+cin»X;
+
+for(int i=0; i<N; i++)
+Ratios[i] = (i+1) / X;
+cout«"Массив: " «"\n" ;
+for(int i=0; i<N; i++)
+cout« Ratios[i] « ' ' ;
+return 0;
+}
