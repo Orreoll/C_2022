69a1d964fa0aab45003ee3c7206a7d1fb913f917
Orreoll <100588277+Orreoll@users.noreply.github.com>
2022-04-26 14:27:56 +0900

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -6,25 +6,28 @@ class Bucket
 public:
 double volume;
 double used;
-    Bucket (int a, int b) {volume = a, used = b;}
+    Bucket (int b, int n) {
+    volume = b, used = n
+    ;}
     void flush(){
-    used = 0;}
-    float fill(float c){
-    float rest = c - (volume - used);
+    used = 0;
+    }
+    double fill(double c){
+    double rest = c - (volume - used);
     used = used + c;
-    if (used >= volume) {
-    used = volume;}
+    if (used >=  volume) {
+    used =  volume;}
     if (rest <= 0){
     rest = 0;}
     return rest;}
 private:};
 int main(){
-    Bucket test_01(20, 4);
-    cout << "1 - " << test_01.volume << ", " << test_01.used << "\n";
-    int rest = test_01.fill(16);
-    cout << "2 - " << test_01.used << ", " << rest << "\n";
-    test_01.flush();
-    rest = test_01.fill(18);
-    cout << "3 - " << test_01.used << ", " << rest << "\n";
+    Bucket mini(12, 3);
+    cout << "1)Обьем в литрах и сколько налито - " << mini.volume << ", " << mini.used << "\n";
+    int rest = mini.fill(14);
+    cout << "2) - " << mini.used << ", " << rest << "\n";
+    mini.flush();
+    rest = mini.fill(8);
+    cout << "3) - " << mini.used << ", " << rest << "\n";
     return 0;
     }
