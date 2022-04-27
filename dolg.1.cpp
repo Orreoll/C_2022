e5e1a8a62893fa8cd33d87f42ab5acb718a066b1
Orreoll <100588277+Orreoll@users.noreply.github.com>
2022-04-27 13:21:22 +0900

    Update dolg.1.cpp

--- dolg.1.cpp
+++ dolg.1.cpp
@@ -22,10 +22,10 @@ used = b;
 int main()
 {
 properties sd_card(256, 175);
-cout « "1)" « sd_card.size « ',' « sd_card.used « "\n";
+cout << "1)" << sd_card.size << ',' << sd_card.used << "\n";
 int t = sd_card.delet(75);
-cout « "2)" « sd_card.used « "\n";
+cout << "2)" << sd_card.used << "\n";
 t = sd_card.write(150);
-cout « "3)" « sd_card.used « "\n";
+cout << "3)" << sd_card.used << "\n";
 return 0;
 }
