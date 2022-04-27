917988a633e1984e77c0523985a712343750dcbc
Orreoll <100588277+Orreoll@users.noreply.github.com>
2022-04-27 13:18:34 +0900

    Update dolg.2.cpp

--- dolg.2.cpp
+++ dolg.2.cpp
@@ -5,16 +5,16 @@ using namespace std;
 int main(){
 int N;
 float X;
-cout«"Введите длину массива N: "«"\n";
-cin»N;
+cout<<"Введите длину массива N: "<<"\n";
+cin>>N;
 float Ratios[N];
-cout«"Введите делитель X: "«"\n";
-cin»X;
+cout<<"Введите делитель X: "<<"\n";
+cin>>X;
 
 for(int i=0; i<N; i++)
 Ratios[i] = (i+1) / X;
-cout«"Массив: " «"\n" ;
+cout<<"Массив: " <<"\n" ;
 for(int i=0; i<N; i++)
-cout« Ratios[i] « ' ' ;
+cout<< Ratios[i] << ' ' ;
 return 0;
 }
